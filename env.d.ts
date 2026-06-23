1c765321216d3a5424d9e2da75cf99cc759b1e1a
강성진 <wlsdljuam@nate.com>
2026-06-23 22:52:21 +0900

    debug.css 추가 env.d.ts 파일도 추가해야합니다.

---
+++ env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="astro/client" />
+
+declare namespace astroHTML.JSX {
+    interface HTMLAttributes {
+        debug?: boolean | string;
+        'debug-all'?: boolean;
+    }
+}
