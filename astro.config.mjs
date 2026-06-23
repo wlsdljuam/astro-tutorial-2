17d31655edd0230bb24bc83569f9d59e07256d52
강성진 <wlsdljuam@nate.com>
2026-06-24 04:01:55 +0900

    개발자 도구모음 테스트

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,24 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import metaTags from "astro-meta-tags";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astrosj.mycafe24.com", // 프로덕션 사이트 URL 지정 import.meta.env.SITE 로 사용할수 있음.
-	compressHTML: false, // false = 빌드 결과물 html 을 보기좋게 포맷팅
-	build: {
-		format: "file", // 빌드 결과물을 ./dist/page.html 형식으로 출력
-		inlineStylesheets: `never`,
-		assets: "assets",
+  // 프로덕션 사이트 URL 지정 import.meta.env.SITE 로 사용할수 있음.
+  site: "https://astrosj.mycafe24.com",
+
+  // false = 빌드 결과물 html 을 보기좋게 포맷팅
+  compressHTML: false,
+
+  build: {
+      format: "file", // 빌드 결과물을 ./dist/page.html 형식으로 출력
+      inlineStylesheets: `never`,
+      assets: "assets",
 	},
-	scopedStyleStrategy: "class", // css 선택자 규칙을 class name 기반으로 출력
+
+  // css 선택자 규칙을 class name 기반으로 출력
+  scopedStyleStrategy: "class",
+
+  integrations: [metaTags()]
 });
