426d8a05bcd7151d1576f585d11a4bac795eec6f
강성진 <wlsdljuam@nate.com>
2026-06-23 23:45:03 +0900

    prettier 적용

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="astro/client" />
 
 declare namespace astroHTML.JSX {
-    interface HTMLAttributes {
-        debug?: boolean | string;
-        'debug-all'?: boolean;
-    }
+	interface HTMLAttributes {
+		debug?: boolean | string;
+		"debug-all"?: boolean;
+	}
 }
